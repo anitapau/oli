5b1f36d33d9da2a69baefcb573f2b42b4314b3b0
anitapau <anitapau@uw.edu>
2017-12-14 12:04:39 -0800

    take photo implemented without the feature of saving image

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,56 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { MediaCapture } from '@ionic-native/media-capture';
-import { Camera } from '@ionic-native/camera';
+import {Camera, CameraOptions} from "@ionic-native/camera";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  @ViewChild('myvideo') myVideo: any;
-  constructor(public navCtrl: NavController) {
+  public photos: any;
+  public base64Image:string;
+
+  ngOnInit(){
+    this.photos=[];
   }
+  takePhoto() {
+    const options: CameraOptions={
+      quality:50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera
+      .getPicture(options).then(imageData=> {
+      this.base64Image = "data:image/jpeg;base64," + imageData;
+      this.photos.push(this.base64Image);
+      //reverse is to change the order of display to user; latest will be shown first
+
+      this.photos.reverse();
+      this.sendData(ImageData);
+    }, err=> {console.log(err);});}
+
+
+
+
+  //delete the image based on the index
+
+  deletePhoto(index) {
+    alert("delete photo");
+  }
+
+
+
+
+
+
+
+
+  @ViewChild('myvideo') myVideo: any;
+  constructor(public navCtrl: NavController, private camera:Camera) {
+  }
+
 
   startrecording(){
     new MediaCapture().captureVideo((videodata) => {
